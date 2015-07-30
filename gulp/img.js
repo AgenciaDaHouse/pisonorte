bdcf9e1862e4a429bb7165fe6992157667bce6a7
Renan Couto <renancouto@gmail.com>
2015-07-29 21:46:41 -0300

    added - favicon

--- gulp/img.js
+++ gulp/img.js
@@ -6,9 +6,17 @@
 var gulp = require('gulp')
 
 /**
+ * favicon task
+ */
+gulp.task('favicon', function () {
+  return gulp.src('./src/img/favicon/**')
+    .pipe(gulp.dest('./public'))
+})
+
+/**
  * images task
  */
-gulp.task('img', function () {
+gulp.task('img', [ 'favicon' ], function () {
   return gulp.src('./src/img/*.{jpg,png,gif,svg,ico}')
     .pipe(gulp.dest('./public/img'))
 })
