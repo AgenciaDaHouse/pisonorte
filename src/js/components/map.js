a8d1a948e57c32fddd4d2ee76c9eeef7e3a334ca
Renan Couto <renancouto@gmail.com>
2015-09-04 07:50:21 -0300

    added - second store address

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -3,19 +3,31 @@
 'use strict'
 
 function initialize() {
-  var myLatlng = new google.maps.LatLng(-23.259513, -51.163848)
+  var ll1 = new google.maps.LatLng(-23.259513, -51.163848)
+  var ll2 = new google.maps.LatLng(-23.315396, -51.151530)
   var mapOptions = {
     zoom: 15,
-    center: myLatlng,
     scrollwheel: false
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
-  var marker = new google.maps.Marker({
-    position: myLatlng,
+
+  var marker1 = new google.maps.Marker({
+    position: ll1,
     map: map,
-    title: 'TS&B Contractors Ltd'
+    title: 'Pisonorte'
   })
+  var marker2 = new google.maps.Marker({
+    position: ll2,
+    map: map,
+    title: 'Pisonorte'
+  })
+
+  var bounds = new google.maps.LatLngBounds()
+  bounds.extend(marker1.position)
+  bounds.extend(marker2.position)
+
+  map.fitBounds(bounds)
 }
 
 google.maps.event.addDomListener(window, 'load', initialize)
