cf6bf28f4590c975bf9bf3dc9cbc915bac268ac5
Renan Couto <renancouto@gmail.com>
2016-01-08 14:10:03 -0200

    updated - address

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -3,31 +3,20 @@
 'use strict'
 
 function initialize() {
-  var ll1 = new google.maps.LatLng(-23.259513, -51.163848)
-  var ll2 = new google.maps.LatLng(-23.315396, -51.151530)
+  var ll = new google.maps.LatLng(-23.259545, -51.163688)
   var mapOptions = {
+    center: ll,
     zoom: 15,
     scrollwheel: false
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
 
-  var marker1 = new google.maps.Marker({
-    position: ll1,
+  var marker = new google.maps.Marker({
+    position: ll,
     map: map,
     title: 'Pisonorte'
   })
-  var marker2 = new google.maps.Marker({
-    position: ll2,
-    map: map,
-    title: 'Pisonorte'
-  })
-
-  var bounds = new google.maps.LatLngBounds()
-  bounds.extend(marker1.position)
-  bounds.extend(marker2.position)
-
-  map.fitBounds(bounds)
 }
 
 google.maps.event.addDomListener(window, 'load', initialize)
